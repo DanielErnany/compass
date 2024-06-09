7ac979ae16038957026cb67ebe55884fa669005a
DanielErnany <ddernany@gmail.com>
2024-06-09 17:24:46 -0300

    configure android data so that the app can run

--- android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -2,5 +2,22 @@ package com.example.verygoodcore.compass
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+import android.os.Bundle
+import androidx.annotation.NonNull
+import android.view.MotionEvent
+
+import com.samsung.wearable_rotary.WearableRotaryPlugin
+
+class MainActivity: FlutterActivity() { 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        intent.putExtra("background_mode", "transparent")
+        super.onCreate(savedInstanceState)
+    }
+
+        override fun onGenericMotionEvent(event: MotionEvent?): Boolean {
+        return when {
+            WearableRotaryPlugin.onGenericMotionEvent(event) -> true
+            else -> super.onGenericMotionEvent(event)
+        }
+    }
 }
